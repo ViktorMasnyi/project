2480147a0f639f62c2553e492ca932660ba9dc06
Viktor Masniy <vmasney@gmail.com>
2017-11-29 16:49:00 +0200

    Foogle map was added to Contacts and Image GAllery was fixed with the img urls

--- src/contacts/Contacts.jsx
+++ src/contacts/Contacts.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import Gallery from "../gallery/Gallery";
+import MyMapComponent from "../contacts/MyMapComponent"
 
-let urls = [
-  '../gallery/img/1.JPG',
-  '../gallery/img/2.JPG',
-  '../gallery/img/3.JPG'
+let googlemapkey="AIzaSyCZ1mb117y5IUWlYqHRpajl6bxhrCQeuGI";
+
+let urls=[
+  'img/1.JPG',
+  'img/2.JPG',
+  'img/3.JPG',
+  'img/4.JPG',
+  'img/5.JPG',
+  'img/1.JPG',
+  'img/2.JPG',
+  'img/3.JPG',
+  'img/4.JPG',
+  'img/5.JPG'
 ];
 
 const Contacts = () => (
   <div className="contacts">
     this is the Contact component
     <Gallery imageUrls={urls}/>
+    <MyMapComponent
+      isMarkerShown
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZ1mb117y5IUWlYqHRpajl6bxhrCQeuGI&v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
   </div>
 );
 
